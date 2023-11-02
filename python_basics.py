0949cd3a7a68664b2d0987ab063e0b318e0b8352
Ujwal2607 <145640919+Ujwal2607@users.noreply.github.com>
2023-11-02 09:08:18 +0530

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,48 @@
+# Single line commenting- it can be done by pressing shift+3 key
+
+# Single line commenting- or it can be done by pressing ctrl+? and if you press ctr+? on commented line it gets uncommented
+
+"""
+Multiple line commenting- it is represented or enclosed between triple "
+welcome to python class
+Basics of python
+"""
+
+# Assignment operator
+
+a = 10                 # a is variable and 10 is value of variable a
+print(a)
+print("The value of a is ", a)
+print("The value of a:{}".format(a))
+
+b = 5
+print("The value of a is {} and the value of b is {}".format(a, b))
+
+# python memory space is managed by heap space memory management
+
+a = 20
+print("The value of a is after replacing is {}".format(a))
+
+# sum of a and b
+
+sum_a_b = a+b
+print("The sum of a and b is {}".format(sum_a_b))
+
+# difference of a and b
+
+diff_a_b = a-b
+print("The difference of a and b is {}".format(diff_a_b))
+
+# product of a and b
+prod_a_b = a*b
+print("The product of a and b is {}".format(prod_a_b))
+
+# To get Quotient
+quot_a_b = a/b
+print("The quotient when a is divided by b is {}".format(quot_a_b))
+
+# To get reminder
+rem_a_b = a%b
+print("The reminder when a is divided by b is {}".format(rem_a_b))
+
+
