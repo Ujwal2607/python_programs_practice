1999ae267f45266a52a966ba72fbe1fa400f6d38
Ujwal2607 <145640919+Ujwal2607@users.noreply.github.com>
2023-11-05 22:30:48 +0530

    Add files via upload

---
+++ practice_string.py
@@ -0,0 +1,86 @@
+empty_string = ""
+str_a = "python"
+str_n = "10"
+str_an = "python4.0"
+str_s = " "
+
+# How to find the length of string
+
+len_str = len(str_a)
+print("The length of string is {}".format(len_str))
+
+type_str = type(str_an)
+print("The type of variable is {}".format(type_str))
+
+# Indexing
+
+str_x = "python"
+
+strx_zeroth_ind = str_x[0]
+print("The zeroth index value of strx is {}".format(strx_zeroth_ind))
+
+strx_fst_ind = str_x[1]
+print("The fst index value of strx is {}".format(strx_fst_ind))
+
+str_x = " "
+
+strx_zeroth_ind = str_x[0]
+print("The zeroth index value of strx is {}".format(strx_zeroth_ind))
+
+# Assignment
+
+# Find length and index
+
+str_y = "welcome to class"
+
+len_str = len(str_y)
+print("The length of string is {}".format(len_str))
+
+
+stry_zeroth_ind = str_y[0]
+print("The zeroth index value of stry is {}".format(stry_zeroth_ind))
+
+stry_fst_ind = str_y[1]
+print("The fst index value of stry is {}".format(stry_fst_ind))
+
+stry_snd_ind = str_y[2]
+print("The snd index value of stry is {}".format(stry_snd_ind))
+
+stry_trd_ind = str_y[3]
+print("The trd index value of stry is {}".format(stry_trd_ind))
+
+stry_fourth_ind = str_y[4]
+print("The fourth index value of stry is {}".format(stry_fourth_ind))
+
+stry_fifth_ind = str_y[5]
+print("The fifth index value of stry is {}".format(stry_fifth_ind))
+
+stry_sixth_ind = str_y[6]
+print("The sixth index value of stry is {}".format(stry_sixth_ind))
+
+stry_svth_ind = str_y[7]
+print("The svth index value of stry is {}".format(stry_svth_ind))
+
+stry_eighth_ind = str_y[8]
+print("The eighth index value of stry is {}".format(stry_eighth_ind))
+
+stry_ninth_ind = str_y[9]
+print("The ninth index value of stry is {}".format(stry_ninth_ind))
+
+stry_tenth_ind = str_y[10]
+print("The tenth index value of stry is {}".format(stry_tenth_ind))
+
+stry_eleventh_ind = str_y[11]
+print("The eleventh index value of stry is {}".format(stry_eleventh_ind))
+
+stry_twth_ind = str_y[12]
+print("The twth index value of stry is {}".format(stry_twth_ind))
+
+stry_thirteenth_ind = str_y[13]
+print("The thirteenth index value of stry is {}".format(stry_thirteenth_ind))
+
+stry_fourteenth_ind = str_y[14]
+print("The fourteenth index value of stry is {}".format(stry_fourteenth_ind))
+
+stry_fifteenth_ind = str_y[15]
+print("The fifteenth index value of stry is {}".format(stry_fifteenth_ind))
